c99ae2a5657c1bf0b5be06ec8d8b140d977461c4
Maks <crazy13maks@gmail.com>
2018-09-25 22:53:31 +0300

    Added typeConverter

---
+++ Parser/TypeConverter.cs
@@ -0,0 +1,37 @@
+﻿using Parser.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parser
+{
+    public class TypeConverter
+    {
+        public static SourceType GetSourceType(String type)
+        {
+            type = type.ToLower();
+            switch (type)
+            {
+                case "rss": return SourceType.Rss;
+                case "xml": return SourceType.XML;
+                case "atom": return SourceType.Atom;
+                case "html": return SourceType.HTML;
+                default:
+                    return SourceType.Unknown;
+            }
+        }
+        public static OutputType GetOutputType(String type)
+        {
+            type = type.ToLower();
+            switch (type)
+            {
+                case "rss": return OutputType.Rss;
+                case "atom": return OutputType.Atom;
+                case "xml": return OutputType.XML;
+
+                default:
+                    return OutputType.Rss;
+            }
+        }
+    }
+}
